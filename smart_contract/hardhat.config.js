e3a3d9934e8400cc4fc8680c73b8e27a23becee2
marvelmiles <marvellousabidemi2@gmail.com>
2024-05-14 06:10:23 +0100

    added scroll down toast

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,14 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: '0.8.0',
+  solidity: "0.8.0",
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/8a9bc9660fb1460a9cdbdfedb9612b1c",
+      chainId: 11155111,
+      accounts: [
+        "ec30b5144ba8004b688372e5aa9fce280dc5dc7ba73efc2c39a7d9f47174de7f",
+      ],
     },
   },
 };
